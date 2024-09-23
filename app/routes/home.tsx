335d5a81aa45f978dfe1fb5a262b986fcc6a182a
Takuya Yonezawa <takuya.yonezawa@japan-d2.com>
2024-09-24 02:26:20 +0900

    Update tailwind.css background colors

---
+++ app/routes/home.tsx
@@ -0,0 +1,54 @@
+import type { MetaFunction } from "@remix-run/node";
+import { DatePicker, Typography } from "antd";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { PrismaClient } from "@prisma/client";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export const loader = async () => {
+  const prisma = new PrismaClient();
+
+  const todoItems = await prisma.todoItem.findMany();
+
+  return todoItems;
+};
+
+export default function Index() {
+  const products = useLoaderData<typeof loader>();
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-16">
+        <header className="flex flex-col items-center gap-9">
+          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+            Welcome to <span className="sr-only">Remix</span>
+          </h1>
+          <Typography.Text code>{JSON.stringify(products)}</Typography.Text>
+          <div className="h-[144px] w-[434px]">
+            <DatePicker />
+            <img
+              src="/logo-light.png"
+              alt="Remix"
+              className="block w-full dark:hidden"
+            />
+            <img
+              src="/logo-dark.png"
+              alt="Remix"
+              className="hidden w-full dark:block"
+            />
+          </div>
+        </header>
+        <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
+          <p className="leading-6 text-gray-700 dark:text-gray-200">
+            What&apos;s next?
+          </p>
+        </nav>
+      </div>
+    </div>
+  );
+}
