89d5e575f7c31a25ac63b73f344f6fc7d1a2bb12
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2024-09-28 11:52:14 +0900

    up

---
+++ app/routes/todo.create.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef } from "react";
+import type { MetaFunction, LoaderFunction } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+import { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
+
+export let meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+export async function action({ request }: ActionFunctionArgs) {
+  //   let formData = await request.formData();
+  //   let title = formData.get("title");
+  //   console.log(title);
+  //   //db
+  //   const prisma = new PrismaClient();
+  //   const result = await prisma.task.create({
+  //     data: {
+  //       title: title,
+  //       content: "",
+  //       userId: 0,
+  //     },
+  //   });
+  //   await prisma.$disconnect();
+  return json({ result: "OK" });
+}
+export default function Page() {
+  //   let data = useActionData();
+  //   console.log(data);
+  //console.log(cfg.OK_CODE);
+
+  return (
+    <div className="remix__page">
+      <main>
+        <h2>Task - Create</h2>
+        <hr />
+        <Form method="post" name="form3" id="form3" className="remix__form">
+          <label>
+            <div>title:</div>
+            <input name="title" id="title" type="text" />
+          </label>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </Form>
+      </main>
+    </div>
+  );
+}
--- app/routes/todo.tsx
+++ app/routes/todo.tsx
@@ -135,28 +135,28 @@ export default function Index() {
           <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
             Welcome to <span className="sr-only">Remix</span>
           </h1>
-          <Typography.Text code>{JSON.stringify(todoItems)}</Typography.Text>
-          {todoItems.map((item, index) => {
-            return (
-              <div key={item.id}>
-                <TodoItemForm item={item} />
-              </div>
-            );
-          })}
-          <div className="h-[144px] w-[434px]">
-            <DatePicker />
-            <img
-              src="/logo-light.png"
-              alt="Remix"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src="/logo-dark.png"
-              alt="Remix"
-              className="hidden w-full dark:block"
-            />
-          </div>
         </header>
+        <Typography.Text code>{JSON.stringify(todoItems)}</Typography.Text>
+        {todoItems.map((item, index) => {
+          return (
+            <div key={item.id}>
+              <TodoItemForm item={item} />
+            </div>
+          );
+        })}
+        <div className="h-[144px] w-[434px]">
+          <DatePicker />
+          <img
+            src="/logo-light.png"
+            alt="Remix"
+            className="block w-full dark:hidden"
+          />
+          <img
+            src="/logo-dark.png"
+            alt="Remix"
+            className="hidden w-full dark:block"
+          />
+        </div>
         <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
           <p className="leading-6 text-gray-700 dark:text-gray-200">
             What&apos;s next?
